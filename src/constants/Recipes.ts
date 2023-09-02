47323c07eedcf77bf4a43f9183ecd6bb8a938251
zaiem <zaiem.180410116045@gmail.com>
2023-09-02 16:48:07 -0400

    add(service): add to shopping list

--- src/constants/Recipes.ts
+++ src/constants/Recipes.ts
@@ -1,24 +1,46 @@
+import { Ingredient } from 'src/models/ingredient.model';
 import { Recipe } from '../models/recipe.model';
+import { ingredients } from './Ingredients';
 
 export const recipes: Recipe[] = [
   new Recipe(
     'Chicken Tikka',
     'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti quod iure eius?',
-    'https://hips.hearstapps.com/hmg-prod/images/chicken-tikka-masala1-1663341991.jpg'
+    'https://hips.hearstapps.com/hmg-prod/images/chicken-tikka-masala1-1663341991.jpg',
+    [
+      new Ingredient('chicken', 1, ingredients.length),
+      new Ingredient('curd', 3, ingredients.length),
+      new Ingredient('tomatoes', 4, ingredients.length),
+    ]
   ),
   new Recipe(
     'Butter Chicken',
     'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti quod iure eius?',
-    'https://cafedelites.com/wp-content/uploads/2019/01/Butter-Chicken-IMAGE-64.jpg'
+    'https://cafedelites.com/wp-content/uploads/2019/01/Butter-Chicken-IMAGE-64.jpg',
+    [
+      new Ingredient('chicken', 1, ingredients.length),
+      new Ingredient('cream', 3, ingredients.length),
+      new Ingredient('onion', 4, ingredients.length),
+    ]
   ),
   new Recipe(
     'Paneer Tikka',
     'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti quod iure eius?',
-    'https://www.indianveggiedelight.com/wp-content/uploads/2021/08/air-fryer-paneer-tikka-featured.jpg'
+    'https://www.indianveggiedelight.com/wp-content/uploads/2021/08/air-fryer-paneer-tikka-featured.jpg',
+    [
+      new Ingredient('paneer', 1, ingredients.length),
+      new Ingredient('curd', 3, ingredients.length),
+      new Ingredient('mirchi', 4, ingredients.length),
+    ]
   ),
   new Recipe(
     'Masala Dosa',
     'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti quod iure eius?',
-    'https://hips.hearstapps.com/hmg-prod/images/chicken-tikka-masala1-1663341991.jpg'
+    'https://hips.hearstapps.com/hmg-prod/images/chicken-tikka-masala1-1663341991.jpg',
+    [
+      new Ingredient('batter', 1, ingredients.length),
+      new Ingredient('powder', 3, ingredients.length),
+      new Ingredient('sambhar', 4, ingredients.length),
+    ]
   ),
 ];
--- src/services/recipe.service.ts
+++ src/services/recipe.service.ts
@@ -1,5 +1,7 @@
 import { Injectable, EventEmitter } from '@angular/core';
+import { ingredients } from 'src/constants/Ingredients';
 import { recipes } from 'src/constants/Recipes';
+import { Ingredient } from 'src/models/ingredient.model';
 import { Recipe } from 'src/models/recipe.model';
 
 @Injectable()
@@ -9,5 +11,21 @@ export class RecipeService {
 
   getRecipes() {
     return [...this.recipesData];
+  }
+
+  addToShoppoingList(recipeIngredients: Ingredient[]) {
+    recipeIngredients.forEach((recipeIng) => {
+      const isIngredientPresent = ingredients.find(
+        (ingredient) => ingredient.name === recipeIng.name
+      );
+
+      if (!!isIngredientPresent) {
+        ingredients[isIngredientPresent.id].amount += recipeIng.amount;
+      } else {
+        ingredients.push(
+          new Ingredient(recipeIng.name, recipeIng.amount, ingredients.length)
+        );
+      }
+    });
   }
 }
